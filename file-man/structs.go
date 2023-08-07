36cfea8fec3da33b0b62e52d9e9bb30dc451e1d1
aquav3 <kyrbynyt@gmail.com>
2023-08-07 05:18:49 +0300

    Code organization :DDDDD

---
+++ file-man/structs.go
@@ -0,0 +1,14 @@
+package fileman
+
+type Command string
+
+const (
+    Create Command = "create"
+    Read Command = "read"
+    Delete Command = "delete"
+    Ls Command = "ls"
+    Cd Command = "cd"
+    Pwd Command = "pwd"
+    Help Command = "help"
+    Exit Command = "exit"
+)
--- main.go
+++ main.go
@@ -3,23 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
-
+    "github.com/aquav3/file-manager/file-man"
 	"github.com/aquav3/file-manager/utils"
 )
 
-type Command string
-
-const (
-    Create Command = "create"
-    Read Command = "read"
-    Delete Command = "delete"
-    Ls Command = "ls"
-    Cd Command = "cd"
-    Pwd Command = "pwd"
-    Help Command = "help"
-    Exit Command = "exit"
-)
+ 
 
 func main() {
-
+    
 }
